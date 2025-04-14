02d1f01d467e01fc5e6d30542d694dc24f3a2592
charlesmarquant <marquantcharles@gmail.com>
2025-04-14 16:08:39 -0400

    connect to DB

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import pandas as pd
 
 ## Connect to MySQL database
 connectDB = dbutil.connect_to_mysql()
+cursor = connectDB.cursor()
 
+## Read staging table
+# stgPokemonData = []
+sql = "SELECT * FROM stgPokemon"
+# cursor.execute(sql)
+# columns = cursor.description 
+# for value in cursor.fetchall(): 
+#     tmp = {}
+#     for (index, column) in enumerate(value): 
+#         tmp[columns[index][0]] = column
+#     stgPokemonData.append(tmp)
+# print(stgPokemonData)
 
+stgPokemonData = pd.read_sql_query(sql, connectDB)
+print(stgPokemonData)
 
