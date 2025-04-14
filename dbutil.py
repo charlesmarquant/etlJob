394b6ac1c69155a993f103a655eb4bb538189e44
charlesmarquant <marquantcharles@gmail.com>
2025-04-14 15:04:07 -0400

    init project

---
+++ dbutil.py
@@ -0,0 +1,46 @@
+import pymysql
+
+
+def read_config():
+    ## Load the database configuration from a file  
+    config = {}
+    with open("/Users/carlito/Desktop/python/apiToDB/db_config.txt", "r") as file:
+        for line in file:
+            key, value = line.strip().split("=")
+            config[key] = value
+    return config
+
+def connect_to_mysql():
+    ## Connect to the database 
+    dbconf = read_config()
+    try:
+        connection = pymysql.connect(
+            charset=dbconf["charset"],
+            connect_timeout=int(dbconf["timeout"]),
+            cursorclass=pymysql.cursors.DictCursor,
+            db=dbconf["db"],
+            host=dbconf["host"],
+            password=dbconf["password"],
+            read_timeout=int(dbconf["timeout"]),
+            port=int(dbconf["port"]),
+            user=dbconf["user"],
+            write_timeout=int(dbconf["timeout"])
+        )
+        if connection.open:
+            print("Successfully connected to the database")
+            return connection
+    except pymysql.connect.Error as err:
+        print(f"Error: {err}")
+        return None
+    
+
+def insert_to_mysql(connection, sql):
+    ## Insert data into the database
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+            connection.commit()
+            print("Data inserted successfully")
+    except pymysql.MySQLError as err:
+        print(f"Error: {err}")
+        connection.rollback()
---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+import dbutil
+import pandas as pd
+
+## Connect to MySQL database
+connectDB = dbutil.connect_to_mysql()
+
+
+
